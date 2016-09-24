6c0e0a998bcd50aac523a2d374b55a0818d304da
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-09-24 21:26:27 +0000

    Databse created and templates html page created.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, flash, session, redirect, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
+from model import connect_to_db
+
 from jinja2 import StrictUndefined
 
 
@@ -58,7 +60,7 @@ def index():
 if __name__ == "__main__":
 
     app.debug = True
-    # connect_to_db(app)
+    connect_to_db(app)
     DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
